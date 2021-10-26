35cccdc6f1a939e9e1fbc0a7e5bfb7f766eb7a2d
Kedev <80370389+cs-AI-dev@users.noreply.github.com>
2021-10-26 12:53:15 -0500

    Create python_3_install.py

---
+++ master/install/windows/python_3_install.py
@@ -0,0 +1,15 @@
+import os
+try:
+	os.system("python pip install tk -q")
+except:
+	print("[ERROR] Error installing TKinter for DS-PVM installation GUI.")
+	exit()
+	
+from tkinter import *
+	
+master = Tk()
+master.configure(bg="black")
+master.title("Dragonscale Python Virtual
+
+if __name__ == "__main__":
+	master.mainloop()
